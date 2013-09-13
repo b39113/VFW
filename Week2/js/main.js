f3457d960a9f8423c6df6f76579249e4103cb2de
b39113 <Boyd@Boyds-MacBook-Pro.local>
2013-09-12 20:32:37 -0500

    Updates

--- Week2/js/main.js
+++ Week2/js/main.js
@@ -11,16 +11,19 @@ window.addEventListener("DOMContentLoaded", function(){
 	
 	
 	// Variables
+/*
 	var ideaTitle = $('ideaTitle');
 	var importance = $('importance');
 	var dateDue = $('dateDue');
 	var description = $('description');
 	var category = $('category');
 	var clearAllLink = $('clearAllLink');
+	var displayLink = $('displayLink');
 	var status = document.getElementById("mySubmit").status;
-	
-	console.log(category);
+	// This is not pulling category or status back as an array, I am stuck on this
+	console.log(status);
 	console.log(localStorage);
+*/
 	
 	
 	
@@ -42,21 +45,69 @@ window.addEventListener("DOMContentLoaded", function(){
 	*/
 	};
 	
+	// Array for creating the Select Fields with JS
+	var statusGroup = ["Lightbulb", "Requirements", "Development", "Implemented"],
+	categoryValue = "No";
+	catCreate();
+	
+	// Create Select Field from JS instead of HTML
+	function catCreate(){
+		var myForm = document.getElementsByTagName("form"),
+			selectLi = $('select'), // Get select element into a var
+			createSelect = document.createElement('select'); //create a select element
+			createSelect.setAttribute("id", "status"); // create an ID attribute and apply to select tag we created
+		for(var i=0, j=statusGroup.length; i<j; i++){
+			var makeOption = document.createElement('option'),
+				optText = statusGroup[i];
+			makeOption.setAttribute("value", optText);
+			makeOption.innerHTML = optText;
+			createSelect.appendChild(makeOption);
+		}
+		selectLi.appendChild(createSelect);
+	};
+	
 	// Clear ALl data function which is called when the button is clicked
 	var clearAll = function(){
 		localStorage.clear();
-	}
+	};
+	
+	// Checkbox Value
+	function getCheckBoxValue(){
+		if($('category').checked){
+			categoryValue = $('category').value;
+		}else{
+			categoryValue = "No";
+		}
+	};
+	
+	// Function to Store Data
+	function storeData (){
+		var id = Math.floor(Math.random()*100000001);
+		// Get all form data in an Object
+		getCheckBoxValue();
+		var idea = {};
+			idea.title = ["Idea Title", $('ideaTitle').value];
+			idea.importance = ["Importance", $('importance').value];
+			idea.dateDue = ["Date Due", $('dateDue').value];
+			idea.description = ["Description", $('description').value];
+			idea.category = ["Category", $('category').value];
+			idea.status = ["Status", $('status').value];
+		localStorage.setItem(id, JSON.stringify(idea));
+		alert("Idea Logged!");
+	};
 	
 	// Function to display items and hide form when button is clicked
-	
+	var displayData = function(){
+		
+	};
 	
 	
 	
 	/* Listeners */
 	
-	butSubmit.addEventListener("click" , captureData);
+	butSubmit.addEventListener("click" , storeData);
 	clearAllLink.addEventListener("click", clearAll);
-	
+	displayLink.addEventListener("click", displayData);
 	
 	
 	
